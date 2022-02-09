a1bb9739837e83d95a37e1bd3813dbca74801119
Philipp Trunschke <philipp.trunschke@univ-nantes.fr>
2022-02-09 16:57:52 +0100

    bugfix

--- install.sh
+++ install.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 ENVNAME="${1}"
+BRANCHNAME="${2:-conda}"
 set -e
 
-if [ -z "${ENVNAME}" ];
-then
-    echo "Usage: bash install.sh <env_name>"
+if [ -z "${ENVNAME}" ]; then
+    echo "Usage: bash install.sh <environment> [<branch>]"
+    echo "       If <branch> is not provided the 'conda' branch will be used."
     exit 0
 fi
 
-read -p "Create new conda environment '${ENVNAME}' (y/n)? " answer
-case ${answer:0:1} in
-    y|Y )
-    ;;
-    * )
-        exit
-    ;;
-esac
+if ! command -v conda &> /dev/null; then
+    echo "Please install conda before running this script."
+    exit 0
+fi
 
-# conda create -n ${ENVNAME} -c conda-forge 'python=3.8' python_abi gxx_linux-64 make 'pip>=18.1' numpy openblas suitesparse lapack liblapacke boost-cpp libgomp scipy matplotlib rich
 eval "$(conda shell.bash hook)"
-conda activate ${ENVNAME}
-NUMPY=${CONDA_PREFIX}/lib/python3.8/site-packages/numpy
-CXX=${CONDA_PREFIX}/bin/x86_64-conda-linux-gnu-c++
 
-git clone --recurse-submodules https://github.com/libxerus/xerus.git --branch SALSA
+REQUIREMENTS="'python=3.8' python_abi gxx_linux-64 make 'pip>=18.1' numpy openblas suitesparse lapack liblapacke boost-cpp libgomp scipy matplotlib rich"
+if test -n "$(conda env list | grep ${ENVNAME})"; then
+    echo "Conda environment '${ENVNAME}' already exists."
+    read -p "Use this environment? (y/n) " answer
+    case ${answer:0:1} in
+        y|Y )
+            conda activate ${ENVNAME}
+            conda install -y -c conda-forge ${REQUIREMENTS}
+        ;;
+        * )
+            exit
+        ;;
+    esac
+else
+    read -p "Create new conda environment '${ENVNAME}'? (y/n) " answer
+    case ${answer:0:1} in
+        y|Y )
+            conda create -y -n ${ENVNAME} -c conda-forge ${REQUIREMENTS}
+            conda activate ${ENVNAME}
+        ;;
+        * )
+            exit
+        ;;
+    esac
+fi
+
+echo "Installing branch 'xerus/${BRANCHNAME}' into environment '${ENVNAME}'."
+
+CXX=${CONDA_PREFIX}/bin/x86_64-conda-linux-gnu-c++
+NUMPY=$(python -c 'import numpy as np; print("/".join(np.__file__.split("/")[:-1]))')
+XERUS=$(python -c 'import numpy as np; print("/".join(np.__file__.split("/")[:-2]))')/xerus.so
+
+cd /tmp
+TEMPDIR=$(mktemp -d)
+cd ${TEMPDIR}
+git clone --recurse-submodules https://github.com/libxerus/xerus.git --branch ${BRANCHNAME}
 cd xerus
 
 cat <<EOF >config.mk
@@ -49,9 +77,15 @@ OTHER += -I${CONDA_PREFIX}/include -I${NUMPY}/core/include/
 OTHER += -L${CONDA_PREFIX}/lib
 EOF
 
-ln -s ${CONDA_PREFIX}/include/ ${CONDA_PREFIX}/include/suitesparse
+if [ ! -d ${CONDA_PREFIX}/include/suitesparse ];
+then
+    ln -s ${CONDA_PREFIX}/include/ ${CONDA_PREFIX}/include/suitesparse
+fi
 make python
+cp build/libxerus_misc.so ${CONDA_PREFIX}/lib/
+cp build/libxerus.so ${CONDA_PREFIX}/lib/
+# cp build/python3/xerus.so ${XERUS}
 python -m pip install . --no-deps -vv
 
-cd ..
-rm -rf xerus
+cd ../..
+rm -rf ${TEMPDIR}
